7b41975ca5d9dd3a7e3714afe1c70cd293a527af
AndriiDyda <Andr.dyda2211@gmail.com>
2022-09-16 17:52:40 +0300

    another commit

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("For fun");
         System.out.println("For fun");
+        System.out.println("For fun");
     }
 }
